f5bb173893d27ca23e622d878959f96b647005cf
sunkyteam <177909664@qq.com>
2025-07-05 19:02:57 +0800

    Add files via upload

---
+++ data/mrmy.js
@@ -0,0 +1,71 @@
+// 名人名言数据
+const quotes = [
+  {
+    id: 1,
+    content: "生活不是等待风暴过去，而是学会在雨中跳舞",
+    author: "维维安·格林",
+    source: ""
+  },
+  {
+    id: 2,
+    content: "最黑暗的夜晚也会迎来黎明",
+    author: "托马斯·富勒",
+    source: ""
+  },
+  {
+    id: 3,
+    content: "成功不是终点，失败也不是终结，唯有勇气才是永恒",
+    author: "温斯顿·丘吉尔",
+    source: ""
+  },
+  {
+    id: 4,
+    content: "梦想不会逃跑，逃跑的永远是自己",
+    author: "宫崎骏",
+    source: "《千与千寻》"
+  },
+  {
+    id: 5,
+    content: "人生没有彩排，每一天都是现场直播",
+    author: "佚名",
+    source: ""
+  },
+  {
+    id: 6,
+    content: "知识就是力量",
+    author: "弗朗西斯·培根",
+    source: ""
+  },
+  {
+    id: 7,
+    content: "三人行，必有我师焉",
+    author: "孔子",
+    source: "《论语》"
+  },
+  {
+    id: 8,
+    content: "天行健，君子以自强不息",
+    author: "周易",
+    source: "《周易》"
+  },
+  {
+    id: 9,
+    content: "生活就像骑自行车，要保持平衡就得不断前进",
+    author: "阿尔伯特·爱因斯坦",
+    source: ""
+  },
+  {
+    id: 10,
+    content: "世上无难事，只怕有心人",
+    author: "王阳明",
+    source: ""
+  },
+  {
+    id: 11,
+    content: "二楼要盖在一楼上",
+    author: "孙上轲",
+    source: ""
+  }
+];
+
+export default quotes;
